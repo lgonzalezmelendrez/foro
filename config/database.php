fa25a88f5c4cb672423412e93ab55ae23a63765a
Luis Armando Gonzalez Melendrez <luis.gonzalez@grupoSLI.sli.mx>
2017-01-19 22:30:53 -0600

    Fix database configuration for Codeship

--- config/database.php
+++ config/database.php
@@ -66,6 +66,20 @@ return [
             'engine' => null,
         ],
 
+         'mysql_tests' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('MYSQL_USER',env('DB_USERNAME', 'forge')),
+            'password' => env('MYSQL_PASSWORD',env('DB_PASSWORD', '')),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', 'localhost'),
